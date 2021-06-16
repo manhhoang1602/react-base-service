0000da3abc16bd5da270356db9209289eef66218
manhhoang1602 <manhhoang1602@gmail.com>
2021-06-16 13:53:06 +0700

    update base service apiCall

---
+++ src/service/Basevices.ts
@@ -0,0 +1,107 @@
+import Axios from "axios"
+import humps from "humps"
+
+export interface IApiResponse<T> {
+    status: number
+    body: T
+}
+
+export interface IMetadata {
+    page: number
+    size: number
+    total: number
+    totalPage: number
+}
+
+const TOKEN_NAME: string = ""
+const baseUrl: string = ""
+const getToken = (): string | null => localStorage.getItem("token")
+
+export const apiCall = async (
+    url: string,
+    method: "GET" | "PUT" | "POST" | "PATCH" | "DELETE",
+    data: { [key: string]: any } | undefined,
+    isToken: boolean
+): Promise<IApiResponse<any>> => {
+    const headers: { [key: string]: string } = {}
+    headers['Content-Type'] = 'application/json'
+    if (isToken) headers[TOKEN_NAME] = getToken() || ""
+
+    return new Promise<any>(resolve => {
+        Axios({
+            url: baseUrl + url,
+            method: method,
+            headers: headers,
+            data: data ? JSON.stringify(data) : undefined
+        })
+            .then(next => {
+                resolve({
+                    body: humps.camelizeKeys(next.data),
+                    status: next.status
+                })
+            })
+            .catch(error => {
+                try {
+                    resolve({
+                        body: humps.camelizeKeys(error.response.data),
+                        status: error.response.error
+                    })
+                } catch (e) {
+                    resolve({
+                        body: e,
+                        status: 500
+                    })
+                }
+            })
+    })
+}
+
+export const getRequest = async (url: string, isToken: boolean = true): Promise<IApiResponse<any>> => {
+    const headers: { [key: string]: string } = {}
+    headers["Content-Type"] = "application/json"
+
+    if (isToken) headers[TOKEN_NAME] = getToken() || ""
+
+    return new Promise<any>(resolve => {
+        Axios.get(
+            baseUrl + url,
+            isToken ? {headers: headers} : undefined
+        )
+            .then(next => {
+                resolve({
+                    body: humps.camelizeKeys(next.data),
+                    status: next.status
+                })
+            })
+            .catch(error => {
+                try {
+                    resolve({
+                        body: humps.camelizeKeys(error.response.data),
+                        status: error.response.status
+                    })
+                } catch (e) {
+                    resolve({
+                        body: e,
+                        status: 500
+                    })
+                }
+            })
+            .finally()
+    })
+}
+
+export const postRequest = (url: string, isToken: boolean = true, data?: { [key: string]: any }): Promise<IApiResponse<any>> => {
+    return apiCall(url, "POST", data, isToken)
+}
+
+export const putRequest = (url: string, isToken: boolean = true, data?: { [key: string]: any }): Promise<IApiResponse<any>> => {
+    return apiCall(url, "PUT", data, isToken)
+}
+
+export const deleteRequest = (url: string, isToken: boolean = true, data?: { [key: string]: any }): Promise<IApiResponse<any>> => {
+    return apiCall(url, "DELETE", data, isToken)
+}
+
+export const patchRequest = (url: string, isToken: boolean = true, data: { [key: string]: any }): Promise<IApiResponse<any>> => {
+    return apiCall(url, "PATCH", data, isToken)
+}
